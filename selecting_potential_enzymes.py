eb090fb3b9ca7d8524f5c741a3558311a6ae7223
Hanna <116320862+queirozhanna@users.noreply.github.com>
2025-02-27 15:34:26 -0300

    Update and rename 270225_Hanna_Extrair_possiveis_enzimas.py to selecting_potential_enzymes.py

--- selecting_potential_enzymes.py
+++ selecting_potential_enzymes.py
@@ -1,11 +1,11 @@
 import csv
 
 # Abrir o arquivo TSV de entrada
-with open('C:/Users/uerjl/OneDrive/Área de Trabalho/Hanna/LITV/DeepGOPlus/Sequências_Tcruzi/Y_C6/Completo_Hanna_210225_DeepGOPlus_Hipoteticas_sem_GO_YC6.tsv', 'r', encoding='utf-8') as infile:
+with open('/path/to/input/file.tsv', 'r', encoding='utf-8') as infile:
     reader = csv.reader(infile, delimiter='\t')
     
     # Abrir um arquivo TSV de saída
-    with open('C:/Users/uerjl/OneDrive/Área de Trabalho/Hanna/LITV/DeepGOPlus/Sequências_Tcruzi/Y_C6/270225_Hanna_Potenciais_Enzimas_YC6.tsv', 'w', encoding='utf-8', newline='') as outfile:
+    with open('/path/to/output/file.tsv', 'w', encoding='utf-8', newline='') as outfile:
         writer = csv.writer(outfile, delimiter='\t')
         
         # Iterar sobre cada linha do arquivo de entrada
